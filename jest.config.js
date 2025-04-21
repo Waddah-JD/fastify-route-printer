8fd129e05ff1d4f1999c8c6677489362c6a6edbb
Waddah <waddah.ja@gmail.com>
2025-04-21 19:34:25 +0300

    add root dir to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   preset: "ts-jest",
+  rootDir: "src",
   testEnvironment: "node",
   transform: {
     "^.+.ts$": ["ts-jest", { useESM: true }],
